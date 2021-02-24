e542aaefa336c69adc1202a6e7d0ba569407a6b4
Matheus Yan Monteiro <matheus.monteiro@confea.local>
2021-02-24 16:33:41 -0300

    updated basic exercise

---
+++ newExercise/Entities/ImportedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace newExercise.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFree { get; protected set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFree ) : base( name, price)
+        {
+            CustomsFree = customsFree;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFree; 
+        }
+
+        public override string PriceTag()
+        {
+
+            return base.PriceTag() + 
+                "(Customs free: $20.00)"
+                +CustomsFree; 
+        }
+    }
+}
---
+++ newExercise/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace newExercise.Entities
+{
+    class Product
+    {
+        public string Name { get;  set; }
+        public double Price { get;  set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name +
+                "$ " +
+                Price;
+        }
+    }
+}
